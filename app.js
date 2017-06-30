915c625249a60c74741fe220b0d8e2a88aa9a6da
Suzal3579 <pe.messh@gmail.com>
2017-06-30 23:42:08 +0545

    Enter page done and data to chat page sent .... body parser took some time .... but worked :D

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let express = require("express"),
     app = express(),
+    bodyParser = require('body-parser'),
     server = require("http").createServer(app),
     io = require("socket.io").listen(server);
 
 app.listen(3000);
 
 // setting up default routes ...
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/jsjquery", express.static(__dirname + "/node_modules/jquery/dist/"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css/"));
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js/"));
@@ -14,4 +17,10 @@ app.use("/index", express.static(__dirname + "/public/"));
 
 app.get("/", (req, res, next) => {
     res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/chat", (req, res, next) => {
+    let name = req.body.name;
+    console.log(name);
+    res.redirect("/");
 });
