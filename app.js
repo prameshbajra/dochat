8cfb751534759396031487767080802cc1403264
Suzal3579 <pe.messh@gmail.com>
2017-07-01 09:16:00 +0545

    Using handlebars for templating engine and modularization done ....

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ let express = require("express"),
     app = express(),
     bodyParser = require('body-parser'),
     server = require("http").createServer(app),
-    io = require("socket.io").listen(server);
+    io = require("socket.io").listen(server),
+    handlebars = require("express-handlebars");
+
+app.engine(".hbs", handlebars({ defaultLayout: "layout", extname: ".hbs", }));
+app.set('view engine', '.hbs');
 
 app.listen(3000);
 
-// setting up default routes ...
+// setting up default routes ... 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/jsjquery", express.static(__dirname + "/node_modules/jquery/dist/"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css/"));
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js/"));
 app.use("/socket.io", express.static(__dirname + "/node_modules/socket.io-client/dist"));
-app.use("/index", express.static(__dirname + "/public/"));
+app.use("/main", express.static(__dirname + "/public/"));
 
 app.get("/", (req, res, next) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.render("index");
 });
 
 app.post("/chat", (req, res, next) => {
     let name = req.body.name;
     console.log(name);
-    res.redirect("/");
+    res.render("chat", { username: name });
 });
---
+++ public/main.js
@@ -0,0 +1,3 @@
+$(document).ready(() => {
+    // access Id and use of socket here ....
+});
